2f3e9d3a3e4af97ea82bd885b65ee6d815c2bfcb
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2020-02-08 21:48:49 -0700

    Added insert statements

--- myDb.sql
+++ myDb.sql
@@ -39,7 +39,7 @@ CREATE TABLE Monsters (
     Monster_Name varchar(255) NOT NULL,
     Monster_Description varchar(255) NOT NULL,
     Monster_Stats varchar(255) NOT NULL,
-    Monster_Loot varchar(255) NOT NULL,
+    Monster_Loot varchar(255) NOT NULL, 
     PRIMARY KEY (Monster_ID)
 );
 CREATE TABLE Items (
@@ -50,3 +50,18 @@ CREATE TABLE Items (
     Item_Price int NOT NULL,
     PRIMARY KEY (Item_ID)
 );
+
+/*Insert default and test data into the tables*/
+INSERT INTO User_Account (User_Name, User_Password)
+VALUES ('John', 'Johnson');
+INSERT INTO Player_Character (Character_Name, Character_Description, Character_Stats)
+VALUES ('Johnny The Swordsman', 'Tall, Handsome, Strong', '18STR, 13DEX, 12INT, 16CON, 14WIS, 16CHA,');
+INSERT INTO Encounters (Encounter_Name, Encounter_Type, Encounter_Description, Encounter_Rewards, Encounter_Result)
+VALUES ('The Swamp', 'Combat', 'An oozing swamp bubbling with ferocious creatures and cretins. A goblin blocks the way.', 
+        'An iron dagger, 50 gold pieces.', 'You beat the goblin!');
+INSERT INTO NPCS (NPC_Name, NPC_Description, NPC_Stats)
+VALUES ('Bob the Blacksmith', 'Muscular, Friendly', '20STR, 12DEX, 11INT, 16CON, 10WIS, 18CHA,');
+INSERT INTO Monsters (Monster_Name, Monster_Description, Monster_Stats, Monster_Loot)
+VALUES ('Goblin', 'Small, Green, Evil', '12STR, ,11DEX, 3INT, 4CON, 12WIS, 8CHA,', 'Iron Dagger, 50 gold pieces');
+INSERT INTO Items (Item_Name, Item_Description, Item_Effect, Item_Price)
+VALUES ('Iron Dagger', 'Small dagger of well wrought iron.', '+2 Dmg, +1 Str', '100 gold pieces');
